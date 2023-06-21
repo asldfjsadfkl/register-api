e5d1546a29e7616ea00ebd487552e6b20980fb5f
Naeem Akhtar <118461098+asldfjsadfkl@users.noreply.github.com>
2023-06-21 18:25:40 +0500

    Update route.js

--- route.js
+++ route.js
@@ -4,7 +4,7 @@ import Schema from "./Database/schema.js";
 const router = express.Router();
 const app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://registratin-i2td.vercel.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
