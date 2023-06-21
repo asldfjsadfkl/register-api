dec9ae07d4003ed7977f216f103dc8e4a53e508f
Naeem Akhtar <118461098+asldfjsadfkl@users.noreply.github.com>
2023-06-21 18:56:16 +0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   })
 );
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://registratin-i2td.vercel.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://registratin-i2td.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
