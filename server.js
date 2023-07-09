61650e590720e63afe636bca9aab46e1dc54494e
Naeem Akhtar <118461098+asldfjsadfkl@users.noreply.github.com>
2023-07-09 10:23:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(
   cors({
      origin: ["https://registratin-i2td.vercel.app", "https://register-api-nine.vercel.app"],
